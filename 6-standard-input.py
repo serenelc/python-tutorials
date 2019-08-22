68e01a9b0eb7834243dfdeaddf09b64f49e54dcd
Serene Chongtrakul <serenelc@outlook.com>
2019-08-22 16:13:33 +0100

    standard input

---
+++ 6-standard-input.py
@@ -0,0 +1,8 @@
+name = input("Tell me your name: ")
+age = input("What is your age: ")
+print(name, "you are", age)
+
+#Function to calculate the area of a circle
+radius = input("What is the radius of your circle: ")
+area = 3.142 * (int(radius) ** 2)
+print("The area is", area)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-tutorials
+
+Following this tutorial: https://www.youtube.com/playlist?list=PL4cUxeGkcC9idu6GZ8EU_5B6WpKTdYZbK
