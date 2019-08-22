a12d78e73dfcae8d088d64828ee56e0fb994cb36
Serene Chongtrakul <serenelc@outlook.com>
2019-08-22 16:46:59 +0100

    while loops

--- 9-for-loops.py
+++ 9-for-loops.py
@@ -1,9 +1,10 @@
-artists = ["Billie Eilish", "Lana Del Rey", "Ariana Grande", "The Beatles"]
+artists = ["Billie Eilish", "The Beatles", "Lana Del Rey",  "Ariana Grande"]
 for a in artists:
     if (a == "The Beatles"):
         print(f"{a} are a great band")
+        break
     else:
         print(f"{a} is a good singer")
 
-for a in artists[:-1]:
+for a in artists[2:]:
     print(f"{a} are solo artists")
