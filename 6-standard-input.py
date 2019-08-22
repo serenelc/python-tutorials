fbe6b19b7ef1be972a52349f789d11dd4864164d
Serene Chongtrakul <serenelc@outlook.com>
2019-08-22 16:23:10 +0100

    string formatting

--- 6-standard-input.py
+++ 6-standard-input.py
@@ -2,7 +2,7 @@ name = input("Tell me your name: ")
 age = input("What is your age: ")
 print(name, "you are", age)
 
-#Function to calculate the area of a circle
+# Function to calculate the area of a circle
 radius = input("What is the radius of your circle: ")
 area = 3.142 * (int(radius) ** 2)
 print("The area is", area)
---
+++ 7-string-formatting.py
@@ -0,0 +1,13 @@
+# Way 1. The comma adds a space of its own.
+name = "Serene"
+surname = "Chongtrakul"
+print("Name:", name, "Surname:", surname)
+
+# Way 2. Format method (numbers correspond to the index of the vars in format(_, _, ...)
+# the .3 is a precision of 3 numbers, .3f means 3 floating point numbers i.e. 3 decimal points
+num1 = 1.2345667
+print("Num1 3 digits: {0:.3} Num1 3 dp: {0:.3f}".format(num1))
+
+# Way 3. Using F-Strings.
+song = "Bury A Friend"
+print(f"I choreographed my routine to {song}")
