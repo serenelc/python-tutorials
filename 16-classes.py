216b06dd9414735020ef0b5fb46ce5cb7579f657
Serene Chongtrakul <serenelc@outlook.com>
2019-08-23 14:01:36 +0100

    init function

--- 16-classes.py
+++ 16-classes.py
@@ -1,16 +1,16 @@
 class Country:
 
-    def __init__(self):
-        self.name = "Thailand"
-        self.capital = "Bangkok"
-        self.language = "Thai"
-        self.population = "60,000,000"
+    def __init__(self, name, capital, language, population):
+        self.name = name
+        self.capital = capital
+        self.language = language
+        self.population = population
 
     def neighbours(self):
         print(f"{self.name} has neighbours Cambodia and Myanmar")
 
-thailand = Country()
-print(f"{thailand.name}'s capital is {thailand.capital}. The majority of the population"
-      f"speak {thailand.language} and the population size is {thailand.population}")
-thailand.neighbours()
 
+thailand = Country("Thailand", "Bangkok", "Thai", "60,000,000")
+print(f"{thailand.name}'s capital is {thailand.capital}. The majority of the population"
+      f" speak {thailand.language} and the population size is {thailand.population}")
+thailand.neighbours()
