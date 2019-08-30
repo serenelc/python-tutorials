ef2388f572ef7b368f3f551a59b9309a069dfa1a
Serene Chongtrakul <serenelc@outlook.com>
2019-08-30 15:11:14 +0100

    visualisation and analytics

--- K-Means-Clustering.py
+++ K-Means-Clustering.py
@@ -15,8 +15,42 @@ train = pd.read_csv(train_url)
 test_url = "http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"
 test = pd.read_csv(test_url)
 
-print("***** Train_Set *****")
-print(train.describe())
-print("\n")
-print("***** Test_Set *****")
-print(test.describe())
+# print("***** Train_Set *****")
+# print(train.head())
+# print("\n")
+# print("***** Test_Set *****")
+# print(test.describe())
+#
+# print(train.columns.values)
+
+# See where values are missing for the data sets. K-Means does not support missing values in the data set we feed it
+# print("*****In the train set*****")
+# print(train.isna().sum())
+# print("\n")
+# print("*****In the test set*****")
+# print(test.isna().sum())
+
+# There are a couple of ways to handle missing values:
+# Remove rows with missing values or Impute missing values
+# Fill missing values with mean column values in the train set (Mean Imputation)
+train.fillna(train.mean(), inplace=True)
+# Fill missing values with mean column values in the test set
+test.fillna(test.mean(), inplace=True)
+
+# There are still missing values in the Cabin and Embarked columns because these values are non numeric
+
+# Survival count with respect to the class
+print(train[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+# Survival count with respect to gender
+print(train[["Sex", "Survived"]].groupby(['Sex'], as_index=False).mean().sort_values(by='Survived', ascending=False))
+
+# Plot age vs survival
+g = sns.FacetGrid(train, col='Survived')
+g.map(plt.hist, 'Age', bins=20)
+# Plot class and age vs survival
+grid = sns.FacetGrid(train, col='Survived', row='Pclass', height=2.2, aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+grid.add_legend()
+
+plt.show()
+
